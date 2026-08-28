e7e171ba1122836191442a0bb8bea1a29eb2d33d
Huluvu424242 <huluvu424242@gmail.com>
2026-08-29 00:46:44 +0200

    feat: lokales Profil in der App anbinden

--- lib/app/taugts_app.dart
+++ lib/app/taugts_app.dart
@@ -1,15 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:taugts/core/theme/app_theme.dart';
 import 'package:taugts/features/home/presentation/home_screen.dart';
+import 'package:taugts/features/profil/models/profil.dart';
+import 'package:taugts/features/profil/services/profil_repository.dart';
 
 class TaugtsApp extends StatelessWidget {
-  const TaugtsApp({super.key});
+  const TaugtsApp({
+    this.profil,
+    this.profilRepository,
+    super.key,
+  });
+
+  final Profil? profil;
+  final ProfilRepository? profilRepository;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const HomeScreen(),
+      home: HomeScreen(
+        profil: profil,
+        profilRepository: profilRepository,
+      ),
       theme: AppTheme.light,
       title: 'Taugt’s?',
     );
