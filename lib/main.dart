83e1bfe604fff3cd3ebebdc14968eee42c467161
Huluvu424242 <huluvu424242@gmail.com>
2026-08-29 00:46:44 +0200

    feat: lokales Profil in der App anbinden

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:taugts/app/taugts_app.dart';
+import 'package:taugts/core/ids/id_generator.dart';
+import 'package:taugts/features/bewertungen/services/datenbank_factory.dart';
+import 'package:taugts/features/profil/services/sqlite_profil_repository.dart';
 
-void main() {
-  runApp(const TaugtsApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final datenbank = await oeffneLokaleDatenbank();
+  final profilRepository = SqliteProfilRepository(
+    datenbank,
+    idGenerator: const UuidGenerator(),
+  );
+  final profil = await profilRepository.ladeOderErstelleProfil();
+  runApp(
+    TaugtsApp(
+      profil: profil,
+      profilRepository: profilRepository,
+    ),
+  );
 }
