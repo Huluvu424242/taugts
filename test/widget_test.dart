d7d1dde594865380f80550a95ab91260a69dd575
Huluvu424242 <huluvu424242@gmail.com>
2026-09-01 08:28:05 +0200

    Story #28: Startnavigation im Widgettest absichern

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -2,10 +2,19 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:taugts/app/taugts_app.dart';
 
 void main() {
-  testWidgets('zeigt die Taugt’s?-Startseite', (tester) async {
+  testWidgets('zeigt die mobile Taugt’s?-Startnavigation', (tester) async {
     await tester.pumpWidget(const TaugtsApp());
 
     expect(find.text('Taugt’s?'), findsOneWidget);
     expect(find.text('Was taugt’s?'), findsOneWidget);
+    expect(find.text('Jetzt bewerten'), findsOneWidget);
+    expect(find.text('Dinge'), findsOneWidget);
+    expect(find.text('Orte'), findsOneWidget);
+    expect(find.text('Bewertungen'), findsOneWidget);
+    expect(find.text('Suche'), findsOneWidget);
+    expect(find.text('Import/Export'), findsOneWidget);
+    expect(find.text('Einstellungen'), findsOneWidget);
+    expect(find.text('Erlebnis registrieren'), findsOneWidget);
+    expect(find.text('Alle Erlebnisse'), findsOneWidget);
   });
 }
