fe53da1fe7de06201a697fc36cc2787ea4e1833b
Huluvu424242 <huluvu424242@gmail.com>
2026-09-01 21:26:13 +0200

    Story #119: PowerShell-Bootstrap lesbarer formatieren

--- tool/bootstrap.ps1
+++ tool/bootstrap.ps1
@@ -2,7 +2,11 @@ $ErrorActionPreference = 'Stop'
 $projectDir = Split-Path -Parent $PSScriptRoot
 Set-Location $projectDir
 
-flutter create --org de.huluvu --project-name taugts --platforms android,windows,linux .
+flutter create `
+  --org de.huluvu `
+  --project-name taugts `
+  --platforms android,windows,linux `
+  .
 flutter pub get
 dart format --set-exit-if-changed lib test
 flutter analyze
