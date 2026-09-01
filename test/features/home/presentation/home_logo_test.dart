10a249bd779bb56d2d52ebcfa532745d91cb26e3
Huluvu424242 <huluvu424242@gmail.com>
2026-09-01 23:24:53 +0200

    Story #136: App-Bar-Logo auf kleinem Bildschirm testen

--- test/features/home/presentation/home_logo_test.dart
+++ test/features/home/presentation/home_logo_test.dart
@@ -4,17 +4,23 @@ import 'package:taugts/core/support/app_support.dart';
 import 'package:taugts/features/home/presentation/home_screen.dart';
 
 void main() {
-  testWidgets('Hauptmenü zeigt kleines Logo ohne höhere App-Bar',
+  testWidgets('Hauptmenü zeigt gut erkennbares KI-Logo ohne höhere App-Bar',
       (tester) async {
+    await tester.binding.setSurfaceSize(const Size(320, 568));
+    addTearDown(() => tester.binding.setSurfaceSize(null));
+
     await tester.pumpWidget(const MaterialApp(home: HomeScreen()));
 
     final logoFinder = find.byKey(const Key('hauptmenue-logo'));
     expect(logoFinder, findsOneWidget);
-    expect(tester.widget<AppLogo>(logoFinder).size, 20);
+    expect(tester.widget<AppLogo>(logoFinder).size, 32);
+    expect(tester.getSize(logoFinder), const Size.square(32));
     expect(tester.getSize(find.byType(AppBar)).height, kToolbarHeight);
 
     final titel = tester.getRect(find.text('Taugt’s?'));
     final logo = tester.getRect(logoFinder);
     expect(logo.left, greaterThan(titel.right));
+    expect(find.byTooltip('App-Menü öffnen'), findsOneWidget);
+    expect(tester.takeException(), isNull);
   });
 }
