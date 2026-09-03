b8d8b9040c3f70a3f33cf2b4d5c3b36f73e08dc2
Huluvu424242 <huluvu424242@gmail.com>
2026-09-03 21:47:28 +0200

    Fix #176: historische Freitextsuche testen

--- test/features/suche/sqlite_suche_service_test.dart
+++ test/features/suche/sqlite_suche_service_test.dart
@@ -84,6 +84,89 @@ void main() {
     );
   });
 
+  test('Historische Suche berücksichtigt den Suchbegriff', () async {
+    final db = LokaleDatenbank.oeffnen(sqlite3.openInMemory());
+    addTearDown(db.schliessen);
+    SqliteKategorieRepository(db);
+    SqliteKlassifikationsRepository(db);
+    final bewertungen = SqliteBewertungsRepository(db);
+    final jetzt = DateTime.utc(2026, 9, 3, 18);
+    const profilId = 'profil-historie';
+    db.verbindung.execute(
+      'INSERT INTO profile VALUES (?, ?, ?, ?)',
+      [profilId, null, jetzt.toIso8601String(), jetzt.toIso8601String()],
+    );
+    await bewertungen.speichereProdukt(Produkt(
+      id: 'p-historie',
+      name: 'Abendstern',
+      erstelltAm: jetzt,
+      geaendertAm: jetzt,
+    ));
+    await bewertungen.speichereOrt(Ort(
+      id: 'o-historie',
+      name: 'Zum Goldenen Hopfen',
+      typ: Ortstyp.gastronomie,
+      erstelltAm: jetzt,
+      geaendertAm: jetzt,
+    ));
+    await bewertungen.speichereErlebnis(Erlebnis(
+      id: 'e-historie',
+      herkunftProfilId: profilId,
+      typ: Erlebnistyp.restaurantbesuch,
+      status: Erlebnisstatus.aktiv,
+      ortId: 'o-historie',
+      tatsaechlicherBeginn: jetzt,
+      erstelltAm: jetzt,
+      geaendertAm: jetzt,
+    ));
+    db.verbindung.execute(
+      '''
+      INSERT INTO erlebnispositionen (
+        id, erlebnis_id, produkt_id, anzahl, erstellt_am, geaendert_am
+      ) VALUES (?, ?, ?, ?, ?, ?)
+      ''',
+      [
+        'pos-historie',
+        'e-historie',
+        'p-historie',
+        1,
+        jetzt.toIso8601String(),
+        jetzt.toIso8601String(),
+      ],
+    );
+    db.verbindung.execute(
+      '''
+      INSERT INTO preisbeobachtungen (
+        id, erlebnis_id, erlebnis_position_id, produkt_id, ort_id,
+        beobachtet_am, betrag_minor, waehrung, erstellt_am, geaendert_am
+      ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+      ''',
+      [
+        'preis-historie',
+        'e-historie',
+        'pos-historie',
+        'p-historie',
+        'o-historie',
+        jetzt.toIso8601String(),
+        490,
+        'EUR',
+        jetzt.toIso8601String(),
+        jetzt.toIso8601String(),
+      ],
+    );
+    final service = SqliteSucheService(db);
+
+    final passend = await service.suche(
+      const Suchfilter(ziel: Suchziel.historie, text: 'Goldenen Hopfen'),
+    );
+    final unpassend = await service.suche(
+      const Suchfilter(ziel: Suchziel.historie, text: 'nicht vorhanden'),
+    );
+
+    expect(passend.map((wert) => wert.id), contains('preis-historie'));
+    expect(unpassend, isEmpty);
+  });
+
   test('Erlebnisse lassen sich nach Typ und Status kombinieren', () async {
     final db = LokaleDatenbank.oeffnen(sqlite3.openInMemory());
     addTearDown(db.schliessen);
