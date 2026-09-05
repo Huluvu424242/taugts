3740371af31256e207b9d2403227c3baf8bbf65e
Huluvu424242 <huluvu424242@gmail.com>
2026-09-05 20:16:17 +0200

    Story #203: Vollständiges Schema in Migration erzeugen

--- lib/features/bewertungen/services/datenbank_factory.dart
+++ lib/features/bewertungen/services/datenbank_factory.dart
@@ -1,14 +1,11 @@
 import 'package:path/path.dart' as p;
 import 'package:path_provider/path_provider.dart';
 import 'package:sqlite3/sqlite3.dart';
-import 'package:taugts/core/persistenz/aktuelles_datenbankschema.dart';
 import 'package:taugts/features/bewertungen/services/lokale_datenbank.dart';
 
 Future<LokaleDatenbank> oeffneLokaleDatenbank() async {
   final verzeichnis = await getApplicationSupportDirectory();
-  final datenbank = LokaleDatenbank.oeffnen(
+  return LokaleDatenbank.oeffnen(
     sqlite3.open(p.join(verzeichnis.path, 'taugts.sqlite')),
   );
-  AktuellesDatenbankschema.stelleFeatureTabellenBereit(datenbank.verbindung);
-  return datenbank;
 }
